696a2ffba773db704cd594c5894593ada4cbbd36
cfunk <christopher.funk@kitware.com>
2020-02-04 14:43:28 -0800

    Adding object detection + running in serial

--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -147,7 +147,7 @@ class Algorithm(object):
         labeled_dataloader = torch.utils.data.DataLoader(
             self.current_dataset,
             sampler=labeled_sampler,
-            batch_size=min(len(self.current_dataset.get_labeled_indices()),
+            batch_size=min(self.current_dataset.labeled_size,
                            int(self.arguments["batch_size"])
                            ),
             num_workers=int(self.arguments["num_workers"]),
@@ -163,7 +163,7 @@ class Algorithm(object):
         unlabeled_dataloader = torch.utils.data.DataLoader(
             self.current_dataset,
             sampler=unlabeled_sampler,
-            batch_size=min(len(self.current_dataset.get_unlabeled_indices()),
+            batch_size=min(self.current_dataset.unlabeled_size,
                            int(self.arguments["batch_size"])
                            ),
             num_workers=int(self.arguments["num_workers"]),
@@ -172,8 +172,8 @@ class Algorithm(object):
         #
         # ###################  End of the Creating DataLoaders ###############
 
-        # ###################  Train Your Approach ###############
-        #  Here you can train your approach.  This approach initializes the
+        # ###################  Find Labels for Active Learning ###############
+        #  Here you can run active learning.  This approach initializes the
         #  networks for each budget level separately though that isn't required.
         #  You could have persistence though different budget levels if you want.
         #  Everything between here and the active learning is where your code
@@ -183,49 +183,49 @@ class Algorithm(object):
         #       unlabeled data (at least be able to run)
 
         # Initialize/Re-Initialize models
+        if self.current_dataset.unlabeled_size > 0:
+            vae = VAAL.model.VAE(int(self.arguments["latent_dim"]))
+            discriminator = VAAL.model.Discriminator(int(self.arguments["latent_dim"]))
 
-        vae = VAAL.model.VAE(int(self.arguments["latent_dim"]))
-        discriminator = VAAL.model.Discriminator(int(self.arguments["latent_dim"]))
+            # train the models on the current data
 
-        # train the models on the current data
+            acc, task_model, vae, discriminator = self.vaal.train(
+                labeled_dataloader,
+                None,  # Setting task model to None so it doesn't train
+                vae,
+                discriminator,
+                unlabeled_dataloader,
+            )
 
-        acc, task_model, vae, discriminator = self.vaal.train(
-            labeled_dataloader,
-            None,  # Setting task model to None so it doesn't train
-            vae,
-            discriminator,
-            unlabeled_dataloader,
-        )
+            #
+            # ###################  End of Label Selection Your Approach ###############
 
-        #
-        # ###################  End of Label Selection Your Approach ###############
+            # ##################  ACTIVE LEARNING... Finally. #####################
+            #  Figure out the current budget left before checkpoint/evaluation from
+            #  the status
+            budget = self.problem.get_current_status['budget_left_until_checkpoint']
 
-        # ##################  ACTIVE LEARNING... Finally. #####################
-        #  Figure out the current budget left before checkpoint/evaluation from
-        #  the status
-        budget = self.problem.get_current_status['budget_left_until_checkpoint']
+            #  This approach sets the budget for how many images that they want
+            #  labeled.
+            self.vaal.sampler = VAAL.sampler.AdversarySampler(budget)
 
-        #  This approach sets the budget for how many images that they want
-        #  labeled.
-        self.vaal.sampler = VAAL.sampler.AdversarySampler(budget)
+             # You pick which indices that you want labeled.  Here, it is using
+             # dataset to ensure the correct indices and tracking inside their
+             # function (the dataset getitem returns the index)
+            sampled_indices = self.vaal.sample_for_labeling(
+                vae, discriminator, unlabeled_dataloader)
 
-#        # You pick which indices that you want labeled.  Here, it is using
-#        # dataset to ensure the correct indices and tracking inside their
-#        # function (the dataset getitem returns the index)
-        sampled_indices = self.vaal.sample_for_labeling(
-            vae, discriminator, unlabeled_dataloader)
-#
-#        #  ########### Query for labels -- Kitware managed ################
-#        #  This function is handled by Kitware and takes the indices from the
-#        #  algorithm and queries for new labels. The new labels are added to
-#        #  the dataset and the labeled/unlabeled indices are updated
-        self.current_dataset.get_more_labels(sampled_indices)
-#        #  Note: you don't have to request the entire budget, but
-#        #      you shouldn't end the function until the budget is exhausted
-#        #      since the budget is lost after evaluation.
+             #  ########### Query for labels -- Kitware managed ################
+             #  This function is handled by Kitware and takes the indices from the
+             #  algorithm and queries for new labels. The new labels are added to
+             #  the dataset and the labeled/unlabeled indices are updated
+            self.current_dataset.get_more_labels(sampled_indices)
+             #  Note: you don't have to request the entire budget, but
+             #      you shouldn't end the function until the budget is exhausted
+             #      since the budget is lost after evaluation.
 
-        #
-        # ##################  End of ACTIVE LEARNING #####################
+            #
+            # ##################  End of ACTIVE LEARNING #####################
 
         # ##################  Training on all labels... #####################
 
@@ -237,7 +237,7 @@ class Algorithm(object):
         labeled_dataloader = torch.utils.data.DataLoader(
             self.current_dataset,
             sampler=labeled_sampler,
-            batch_size=min(len(self.current_dataset.get_labeled_indices()),
+            batch_size=min(self.current_dataset.labeled_size,
                            int(self.arguments["batch_size"])
                            ),
             num_workers=int(self.arguments["num_workers"]),
