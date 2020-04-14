4dd210abf4d3cba2ffa647e4148175041ee3c733
Eric Schumann <eric.schumann@kitware.com>
2020-04-14 14:04:53 -0400

    updated session start to laod the status and metadata for later use

--- protocols.py
+++ protocols.py
@@ -13,13 +13,13 @@ class JPLProtocol(metaclass=abc.ABCMeta):
         self.problem = None
         #TODO: define the data_type
         self.datatype = "full"
-        #TODO: laod the apikey
+        #TODO: load the apikey
         self.apikey = "abcd123"
         self.headers = {'user_secret': self.apikey}
         #TODO laod the url
         self.url
         #TODO find the problem_id
-        self.problem_id = 0
+        self.problem_id = ""
 
     @abc.abstractmethod
     def runProtocol(self):
@@ -90,6 +90,11 @@ class JPLProtocol(metaclass=abc.ABCMeta):
         #prep the headers for easier use in the future.
         self.headers['session_token'] = self.sessiontoken
 
+        # load and store the session status and metadata.
+        self.status = self.get_current_status()
+
+        self.metadata = self.get_problem_metadata()
+
     def getWhitelistsets(self, test_id):
         #TODO: get the whitelist datasets for this test id from the JPL server
         print("getWhitelistsets")
@@ -98,6 +103,7 @@ class JPLProtocol(metaclass=abc.ABCMeta):
 
     def getBudgetCheckpoints(self):
         #TODO: return a list of budget amounts (1 entry per checkpoint)
+        # this can be loaded from self.metadata
         print("getBudgetCheckpoints")
         pass
 
@@ -112,7 +118,110 @@ class JPLProtocol(metaclass=abc.ABCMeta):
         pass
 
     def terminateSession(self):
-        #TODO: end the current session with the JPL server
+        # end the current session with the JPL server
+        # no formal communication with the JPL server is needed to end the session.
+        # wipe the session id so that it can't be inadvertently used again
+        self.sessiontoken = None
+        try:
+            del self.headers['session_token']
+        except KeyError:
+            pass
+        
         print("terminateSession")
         pass
         
+
+    def get_current_status(self):
+        """
+        Get the current status of the session.
+        This will tell you things like the number of images you can query
+        before evaluation, location of the dataset, number of classes, etc.
+        An example session: ::
+
+            {
+                "active": true,
+                "budget_left_until_checkpoint": 3000,
+                "current_dataset": {
+                    "classes": [
+                        "0",
+                        "1",
+                        "2",
+                        "3",
+                        "4",
+                        "5",
+                        "6",
+                        "7",
+                        "8",
+                        "9"
+                    ],
+                    "data_url": "/datasets/lwll_datasets/mnist/mnist_full/train",
+                    "dataset_type": "image_classification",
+                    "name": "mnist",
+                    "number_of_channels": 1,
+                    "number_of_classes": 10,
+                    "number_of_samples_test": 10000,
+                    "number_of_samples_train": 60000,
+                    "uid": "mnist"
+                },
+                "current_label_budget_stages": [
+                    3000,
+                    6000,
+                    8000
+                ],
+                "date_created": 1580411853000,
+                "date_last_interacted": 1580411853000,
+                "pair_stage": "base",
+                "task_id": "problem_test_image_classification",
+                "uid": "iN9xVy67QBt71K9ATOqx",
+                "user_name": "Berkely",
+                "using_sample_datasets": false
+            }
+
+        Returns:
+            dict[str, str]: status of problem/task
+        """
+        r = requests.get(f"{self.url}/session_status", headers=self.headers)
+        r.raise_for_status()
+        status = r.json()['Session_Status']
+
+        return status
+
+    def get_problem_metadata(self):
+        """
+        Get the task metadata from JPL's server.
+        An example: ::
+
+            {
+                "adaptation_can_use_pretrained_model": false,
+                "adaptation_dataset": "mnist",
+                "adaptation_evaluation_metrics": [
+                    "accuracy"
+                ],
+                "adaptation_label_budget": [
+                    1000,
+                    2000,
+                    3000
+                ],
+                "base_can_use_pretrained_model": true,
+                "base_dataset": "mnist",
+                "base_evaluation_metrics": [
+                    "accuracy"
+                ],
+                "base_label_budget": [
+                    3000,
+                    6000,
+                    8000
+                ],
+                "problem_type": "image_classification",
+                "task_id": "problem_test_image_classification"
+            }
+
+        """
+        r = requests.get(
+            f"{self.url}/task_metadata/{self.problem_id}",
+            headers=self.headers)
+        r.raise_for_status()
+        metadata = r.json()
+        return metadata
+
+
