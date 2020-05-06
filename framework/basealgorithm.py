17f217ebd774b1f025cacedbb12fc0bbe97735d5
Christopher Funk <christopher.funk@kitware.com>
2020-05-06 19:47:10 -0400

    Spell checking

--- framework/basealgorithm.py
+++ framework/basealgorithm.py
@@ -16,13 +16,13 @@ algorithm files for researchers to use. """
 class BaseAlgorithm(metaclass=abc.ABCMeta):
     """
     Base Class for defining an algorithm. Implementations of
-    an alogirthm should inherit from this class to be run within
-    the framework. Each implementation must implent the execute
+    an algorithm should inherit from this class to be run within
+    the framework. Each implementation must implement the execute
     function.
 
     An algorithm object is persistent for the duration of the
     test protocol, so meta-information can be assigned to class
-    members (self.xxxx), and will remain avialable for future
+    members (self.xxxx), and will remain available for future
     use.
 
     Attributes:
