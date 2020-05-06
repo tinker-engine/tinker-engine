21dd27b3ab2ae589e84e5ca3bae1f6d362c162b5
Christopher Funk <christopher.funk@kitware.com>
2020-05-06 18:44:37 -0400

    Small formatting changes

--- framework/basealgorithm.py
+++ framework/basealgorithm.py
@@ -6,11 +6,11 @@ import abc
 basealgorithm.py
 ============
 
-The BaseAlgorithm class is a base class that provides an interface for all algorithm implementations
-Each implementation must implement the execute() function. The execute function acts a generic dispatcher
-for calling subfunctions of the algorithm. This is done to simplify the auto-generation of template
-algorithm files for researchers to use.
-"""
+The BaseAlgorithm class is a base class that provides an interface for all 
+algorithm implementations Each implementation must implement the execute() 
+function. The execute function acts a generic dispatcher for calling subfunctions 
+of the algorithm. This is done to simplify the auto-generation of template 
+algorithm files for researchers to use. """
 
 
 class BaseAlgorithm(metaclass=abc.ABCMeta):
@@ -35,25 +35,27 @@ class BaseAlgorithm(metaclass=abc.ABCMeta):
         if isinstance(toolset, dict):
             self.toolset= toolset
         elif toolset:
-            print( "Algorithms must be constructed with dictionary toolset" )
+            print("Algorithms must be constructed with dictionary toolset")
             exit(1)
         else:
             toolset = dict()
 
     @abc.abstractmethod
     def execute(self, toolset, step_descriptor):
-        '''
-        The execute function is a generic dispatch function for handling calls to the algorithm
-        This generic dispatch call allow the introduction of an adapter class for dispatching
-        calls to pre-existing algorithms. This minimizes the changes that need to be made to
-        an existing algorithm to adapt it for use in the framework.
+        """
+        The execute function is a generic dispatch function for handling calls to
+        the algorithm This generic dispatch call allow the introduction of an
+        adapter class for dispatching calls to pre-existing algorithms. This
+        minimizes the changes that need to be made to an existing algorithm to
+        adapt it for use in the framework.
 
         Arguments:
-            toolset: This is a dict of named resources available for this step of execution.
-            step_descriptor: This is a sting describing the desired step of execution to perform
-                This is usually, but not always, the name of the function to call in the derived
-                algorithm class.
-        '''
+            toolset: This is a dict of named resources available for this step of
+                execution.
+            step_descriptor: This is a sting describing the desired step of execution
+                 to perform. This is usually, but not always, the name of the
+                 function to call in the derived algorithm class.
+        """
         raise NotImplementedError
 
 
