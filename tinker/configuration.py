bd97814963f9fc9ca81018c0d02f47f0a35297ab
Roni Choudhury <roni.choudhury@kitware.com>
2020-12-29 11:41:19 -0500

    Remove old config expansion routine

--- tinker/configuration.py
+++ tinker/configuration.py
@@ -5,45 +5,6 @@ from typing import Dict, Any, Iterator, List
 from typing_extensions import TypedDict
 import yaml
 
-from pprint import pprint
-
-
-Replacer = TypedDict("Replacer", {
-    "prefix": str,
-    "iterator": Iterator[Any],
-})
-
-
-def set_dict_path(d: Dict, path: List[str], value: Any) -> Dict:
-    """Set a "deep" value in a dict."""
-    v = d
-    for p in path[:-1]:
-        v = v[p]
-    v[path[-1]] = value
-
-
-def config_walk(prefix: List[str], config: Dict) -> List[Iterator[Any]]:
-    replacers = []
-    skeleton = {}
-
-    for key, value in config.items():
-        new_prefix = prefix + [key]
-
-        if type(value) == dict:
-            if "iterate" in value:
-                set_dict_path(skeleton, new_prefix, None)
-                replacers.append((x for x in zip(value["iterate"], itertools.repeat(new_prefix))))
-            else:
-                replacers += config_walk(new_prefix, value)
-        else:
-            set_dict_path(skeleton, new_prefix, value)
-
-
-    return {
-        "replacers": replacers,
-        "skeleton": skeleton,
-    }
-
 
 def is_iterate(value):
     return type(value) is dict and "iterate" in value
