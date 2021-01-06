e96a23bcd2b323a43250f04895aa4d6b832a5679
Roni Choudhury <roni.choudhury@kitware.com>
2021-01-06 13:16:13 -0500

    Add a test for basic iterate

--- test/test_config.py
+++ test/test_config.py
@@ -1,11 +1,24 @@
 """Test suite for configuration expansion."""
+from click.testing import CliRunner
+import itertools
+from tinker.main import main
 
 
-def test_good():
-    """Demonstrate a passing test."""
-    assert 0 == 0
+def test_iterate():
+    """Test basic `iterate` directive."""
+    runner = CliRunner()
+    result = runner.invoke(main, ["examples/config/show_config.py", "-c", "examples/config/iterate.yaml"])
 
+    # The operation should succeed.
+    assert result.exit_code == 0
 
-def test_bad():
-    """Demonstrate a failing test."""
-    assert 0 == 1
+    # The expected config objects should appear in order.
+    lines = result.output.split("\n")
+    for i, (bar, baz) in enumerate(itertools.product([4, 5, 6], [7, 8])):
+        expected = {
+            "foo": 3,
+            "bar": bar,
+            "baz": baz,
+        }
+
+        assert lines[i] == str(expected)
