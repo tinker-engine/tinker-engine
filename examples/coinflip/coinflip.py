fb5841092665dceb2246aba96f07961ea949c726
Roni Choudhury <roni.choudhury@kitware.com>
2021-03-01 18:46:04 -0500

    Fix linting errors

--- examples/coinflip/coinflip.py
+++ examples/coinflip/coinflip.py
@@ -1,12 +1,25 @@
+"""A Tinker Engine protocol for testing flippable coins."""
+
 from CoinFlipper import WeightedCoin
 import tinker.protocol
 
 
 class CoinFlip(tinker.protocol.Protocol):
+    """
+    A coin flipping protocol.
+
+    It works by constructing a weighted coin of a certain weighting, then
+    flipping it a specific number of times, reporting both the number of heads
+    that came up and the statistically expected number of heads.
+    """
+
     def get_config(self):
+        """Return a default configuration."""
         return {}
 
     def run_protocol(self, config) -> None:
+        """Run the coin flipping experimental protocol."""
+
         # Extract the config values.
         weight = config["weight"]
         trials = config["trials"]
